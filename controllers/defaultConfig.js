b34f449af89fa8c1fef02ff39760b3ebdca4b1aa
liyu123123 <1069393525@qq.com>
2019-08-09 14:00:16 +0800

    base servie

---
+++ controllers/defaultConfig.js
@@ -0,0 +1,12 @@
+//defaultConfig.js
+const config = {
+  // 数据库配置
+  database: {
+      DATABASE: 'scooltable', //数据库名称
+      USERNAME: 'root', //mysql用户名
+      PASSWORD: '123', //mysql密码
+      PORT: '3306', //mysql端口号
+      HOST: 'localhost' //服务器ip
+  }
+}
+module.exports = config
